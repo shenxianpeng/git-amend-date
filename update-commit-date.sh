82234de9d8c2ed324bfcf3f21a20f3e98fbd5cc9
shenxianpeng <xianpeng.shen@gmail.com>
2022-10-16 16:48:15 +0800

    update

--- update-commit-date.sh
+++ update-commit-date.sh
@@ -1,1 +1,14 @@
+# Modify git commit time to non-working hours
+# 
+# For example work hours is 9:00 AM to 6:00 PM from Mon to Fri.
+
+CHANGE_TIME='ture'
+
+weekday=`date '+%A'`
+
+if [ $weekday = "Saturday" ] || [ $weekday = "Sunday" ]; then
+    CHANGE_TIME='false'
+fi
+
 GIT_COMMITTER_DATE="$(date)" git commit --amend --no-edit --date "$(date)"
+echo $GIT_COMMITTER_DATE
